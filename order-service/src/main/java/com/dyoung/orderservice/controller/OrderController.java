3f4a0e717a61017ab582e0f7f6b850f1e4ed70c7
vniko <vnikon69@gmail.com>
2023-08-27 21:39:53 +0300

    [NF] Создан новый сервис inventory-service

--- order-service/src/main/java/com/dyoung/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/dyoung/orderservice/controller/OrderController.java
@@ -4,13 +4,9 @@ import com.dyoung.orderservice.dto.OrderRequest;
 import com.dyoung.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping(value = "/api/order")
 @RequiredArgsConstructor
 public class OrderController {
