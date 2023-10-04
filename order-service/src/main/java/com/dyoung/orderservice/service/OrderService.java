0a7ce62b75504a3d1e90af16bc0c32cf92ebcdef
nikonov <nikonov@rtkrad.ru>
2023-10-04 18:46:11 +0300

    [NF] Добавлена Kafka

--- order-service/src/main/java/com/dyoung/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/dyoung/orderservice/service/OrderService.java
@@ -2,15 +2,15 @@ package com.dyoung.orderservice.service;
 
 import brave.Span;
 import brave.Tracer;
-import com.dyoung.orderservice.client.InventoryClient;
 import com.dyoung.orderservice.dto.InventoryResponse;
 import com.dyoung.orderservice.dto.OrderLineItemsDTO;
 import com.dyoung.orderservice.dto.OrderRequest;
+import com.dyoung.orderservice.event.OrderPlacedEvent;
 import com.dyoung.orderservice.model.Order;
 import com.dyoung.orderservice.model.OrderLineItems;
 import com.dyoung.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,14 +22,11 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 @Transactional
-@Slf4j
 public class OrderService {
-
     private final OrderRepository orderRepository;
-    private final InventoryClient inventoryClient;
     private final WebClient.Builder webClientBuilder;
-
     private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -42,13 +39,10 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemsList().stream()
                 .map(OrderLineItems::getSkuCode)
                 .toList();
-
         Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
-
         try (Tracer.SpanInScope isLookup = tracer.withSpanInScope(inventoryServiceLookup.start())) {
 
             inventoryServiceLookup.tag("call", "inventory-service");
-
             // Call Inventory Service, and place order if product is in
             // stock
             InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()
@@ -57,12 +51,12 @@ public class OrderService {
                     .retrieve()
                     .bodyToMono(InventoryResponse[].class)
                     .block();
-
             boolean allProductsInStock = Arrays.stream(inventoryResponsArray)
                     .allMatch(InventoryResponse::isInStock);
 
             if (allProductsInStock) {
                 orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
                 return "Order Placed Successfully";
             } else {
                 throw new IllegalArgumentException("Product is not in stock, please try again later");
